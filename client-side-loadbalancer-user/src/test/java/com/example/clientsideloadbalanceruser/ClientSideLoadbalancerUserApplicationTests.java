671d9e000c9bd4208f6ece5d116422c71d7a3080
huangxinxin-szu <2011150377@email.szu.edu.cn>
2020-07-07 18:13:21 +0800

    add

---
+++ client-side-loadbalancer-user/src/test/java/com/example/clientsideloadbalanceruser/ClientSideLoadbalancerUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientsideloadbalanceruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSideLoadbalancerUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ loadbalancer-server-say-hello/src/test/java/com/example/loadbalancerserversayhello/LoadbalancerServerSayHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loadbalancerserversayhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancerServerSayHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
