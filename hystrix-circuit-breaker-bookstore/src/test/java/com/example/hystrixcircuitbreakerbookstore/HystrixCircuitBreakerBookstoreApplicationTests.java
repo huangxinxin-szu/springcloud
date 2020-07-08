778aadd2e007976ef89ef779ef08d857fe332e49
huangxinxin-szu <2011150377@email.szu.edu.cn>
2020-07-08 18:09:22 +0800

    add hystrix

---
+++ hystrix-circuit-breaker-bookstore/src/test/java/com/example/hystrixcircuitbreakerbookstore/HystrixCircuitBreakerBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hystrixcircuitbreakerbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixCircuitBreakerBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hystrix-circuit-breaker-reading/src/test/java/com/example/hystrixcircuitbreakerreading/HystrixCircuitBreakerReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hystrixcircuitbreakerreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixCircuitBreakerReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
